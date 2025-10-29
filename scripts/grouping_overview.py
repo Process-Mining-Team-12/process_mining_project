c2bff90b56cb9aa11d49bb45a90aa41a27a74208
Marco <marco.baresi01@gmail.com>
2025-10-29 18:11:49 +0100

    feat(xes): update xes file following merge with main branch

--- scripts/grouping_overview.py
+++ scripts/grouping_overview.py
@@ -1,6 +1,9 @@
+"""
+Visits Grouping Overview
+"""
 # ==============================================
 # 1. TESTS
-# Departments related to laboratory analysis and high-volume test data. 
+# Departments related to laboratory analysis and high-volume test data.
 # This group makes up the majority of the dataset and is kept separate for clarity.
 # ==============================================
 TESTS = {
@@ -16,17 +19,17 @@ TESTS = {
 RADIOLOGY = {
     'RADIOLOGIA': 'RADIOLOGY_DEPT',
     'U.O.S.D. NEURORADIOLOGIA': 'NEURORADIOLOGY',
-    }
+}
 
 # ==============================================
 # 3. SPECIALTY DIAGNOSTIC / INTERVENTIONAL / MEDICAL
-# Outpatient specialty clinics (often labeled 'AMBULATORIO'). 
+# Outpatient specialty clinics (often labeled 'AMBULATORIO').
 # These cover most of the hospital’s diagnostic and specialty services and also
-# treatment and administration of medicines 
+# treatment and administration of medicines
 # ==============================================
 MEDICAL = {
     'GASTROENTEROLOGIA - AMBULATORIO': 'GASTROENTEROLOGY',
-    'NEFROLOGIA - AMBULATORIO':'NEPHROLOGY',
+    'NEFROLOGIA - AMBULATORIO': 'NEPHROLOGY',
     'DERMATOLOGIA E MALATTIE VENEREE - AMBULATORIO': 'DERMATOLOGY',
     'MEDICINA INTERNA - AMBULATORIO': 'INTERNAL_MEDICINE',
     'NEUROLOGIA - AMBULATORIO': 'NEUROLOGY',
@@ -34,7 +37,7 @@ MEDICAL = {
     'REPARTO AMBULATORIALE ALLERGOLOGIA': 'ALLERGOLOGY_AMB',
     "CARDIOLOGIA D'EMERGENZA CON UTIC - AMBULATORIO": 'EMERGENCY_CARDIOLOGY_UTIC',
     'MALATTIE INFETTIVE E TROPICALI A DIREZIONE UNIVERSITARIA - AMBULATORIO - EROGAZIONE FARMACI PER ESTERNI': 'INFECTIOUS_DISEASES_PHARMACY'
-    }
+}
 
 # ==============================================
 # 4. SURGICAL / VASCULAR / ANESTESIA
@@ -51,23 +54,24 @@ SURGERY = {
 # Note from domain expert: Given the nature of the emergency room, probably only surgical consultations, which means that our patient will most likely have this procedure linked to their
 # discharge from the relevant medical department, with a small possibility of microsurgery in the ER
 
-
-#5. Intensive practices 
+# ==============================================
+# 5. Intensive practices
 # Only one category, grouping all cases related to anesthesia & resuscination.
+# ==============================================
 INTENSIVE = {
     'REPARTO AMBULATORIALE ANESTESIA E RIANIMAZIONE': 'ANESTHESIA_RESUSCITATION_AMB',
 }
 
 # ==============================================
 # 6. ONCOLOGY
-# All oncology-related activities, including surgical, hematologic, 
+# All oncology-related activities, including surgical, hematologic,
 # and general outpatient oncology departments.
 # ==============================================
 ONCOLOGY = {
     'REPARTO AMBULATORIALE ONCOLOGIA': 'ONCOLOGY_GENERAL',
     'CHIRURGIA GENERALE ED ONCOLOGICA - AMBULATORIO': 'ONCOLOGY_SURGERY',
     'EMATOLOGIA AD INDIRIZZO ONCOLOGICO - AMBULATORIO': 'ONCOLOGY_HEMATOLOGY',
-    }
+}
 
 
 # ==============================================
